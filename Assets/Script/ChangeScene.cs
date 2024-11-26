c1e36a3160217f261e352eb7d4dd0b86515e0561
chinawin47 <chinawin47@gmail.com>
2024-11-26 23:12:13 +0700

    Change Scene After to pick key

---
+++ Assets/Script/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName; // ชื่อ Scene ที่จะย้ายไป
+
+    public void MoveToScene()
+    {
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName); // ย้ายไปยัง Scene ที่ระบุ
+        }
+        else
+        {
+            Debug.LogError("Scene name is not set in the Inspector!");
+        }
+    }
+}
